d285cdc680ab0526ddb328431b789527859af50d
= <=>
2018-02-24 18:13:14 +0300

    Second commit crypt

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,15 +5,14 @@ import java.sql.Statement;
 import java.util.Scanner;
 
 public class Main {
-    private static    Statement statement;
-    public static void main(String[] args) {
-       scan();
 
-        System.out.println(User.exit());
+    public static void main(String[] args) {
+        scan();
+
 
     }
 
-    private static  void crypt(String user, String password) {
+    private static void crypt(String user, String password) {
 
         StringBuffer su = new StringBuffer(user);
         StringBuffer sp = new StringBuffer(password);
@@ -24,17 +23,41 @@ public class Main {
         Database.update(Database.getStatement(), user, password);
 
 
-
     }
-//    private  static  Map<String, String> unCrypt(String user){
+
+    //    private  static  Map<String, String> unCrypt(String user){
 //
 //    }
-    private  static  void scan(){
+    private static void scan() {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Введите пользователя и пароль");
         String user = sc.nextLine();
-        String password  = sc.nextLine();
-        crypt(user,password);
+        String password = sc.nextLine();
+        System.out.println("Выберите функцию. 1 Шифрование. 2 Дешифрование 3 Выход");
+        int choose;
+        while (true) {
+            choose = sc.nextInt();
+            if (choose == 1) {
+                crypt(user, password);
 
+            }
+            if (choose == 2) {
+                uncrypt(user, password);
+            }
+            if (choose == 3) {
+                break;
+            }
+        }
+    }
+
+    private static void uncrypt(String user, String password) {
+        StringBuffer su = new StringBuffer(user);
+        StringBuffer sp = new StringBuffer(password);
+        System.out.println(su.reverse());
+        System.out.println(sp.reverse());
+        user = String.valueOf(su);
+        password = String.valueOf(sp);
+        Database.execute(Database.getStatement(), user, password);
 
     }
 }
--- src/main/java/database/Database.java
+++ src/main/java/database/Database.java
@@ -29,15 +29,47 @@ public class Database {
     }
     public static void update( Statement statement, String user, String password){
 
-            String query = "INSERT  INTO crypt.users (user, password) VALUES(?,?)";
-        try {
-            PreparedStatement ps = getConnection().prepareStatement(query);
+            String query = "INSERT  INTO  crypt.users  (user, password) VALUES(?,?)";
+        try  (PreparedStatement ps = statement.getConnection().prepareStatement(query)){
             ps.setString(1, user);
             ps.setString(2, password);
             ps.executeUpdate();
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        finally {
+            try {
+                getConnection().close();
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public static  void execute(Statement statement, String user, String password){
+            String query = "SELECT  user, password From users WHERE user = ? AND  password = ?";
 
+
+        try  (PreparedStatement pq  = statement.getConnection().prepareStatement(query)) {
+            pq.setString(1,user);
+            pq.setString(2,password);
+            ResultSet  set = pq.executeQuery();
+           while (set.next()){
+
+               System.out.println(set.getString("user") + " - " + set.getString("password"));
+           }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                getConnection().close();
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
